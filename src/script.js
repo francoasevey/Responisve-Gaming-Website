7fdda8942a3f7f8769ca7e9b92e9d5c564242266
francoromeo <francoromeo5012@gmail.com>
2023-10-04 22:31:16 -0300

    slider

--- src/script.js
+++ src/script.js
@@ -10,5 +10,40 @@ $(document).ready(function() {
         resizeTimer = setTimeout(() => {
             $(document.body).removeClass('resize-animation-stopper');
         }, 400);
+    });
+
+    $('.game-slider').slick({
+        className: "center",
+        arrows: true,
+        centerMode: true,
+        infinite: true,
+        centerPadding: "60px",
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        speed: 500,
+        autoplay: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    dots: false
+                }
+            }
+        ]
     })
-})
+});
